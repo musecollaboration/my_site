db73cb072d9b7451dda2a7b60d0aaae9910553fd
Alexander Terekhov <cahdro2155@gmail.com>
2025-01-27 10:39:47 +0300

    Добавил list_detail.html Во views.py переделал все с применением функции render

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Учебный проект по Django
+
+https://stepik.org/course/114288/info
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 
 def index(request):
-    response = render_to_string('blog/index.html')
-    return HttpResponse(response)
+    return render(request, 'blog/index.html')
 
 
 def posts(request):
-    return HttpResponse('Все посты блога')
+    return render(request, 'blog/list_detail.html')
 
 
 def posts_info(request, name_post: str):
