992d0572126434417c8b969e8f8cdc4fe2568af5
Alexander Terekhov <cahdro2155@gmail.com>
2025-01-30 17:12:34 +0300

    Создал base.html и использовал наследование в blog/index.html Добавил random вывод 3х постов

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,59 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import sample
+
+posts_lst = [
+    {
+        'title': 'Рыбалка',
+        'description': 'Хорошо посидели',
+        'date': '21 авг 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+    },
+    {
+        'title': 'Париж',
+        'description': 'Незабываемое путешествие',
+        'date': '5 сент 2020',
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+    },
+    {
+        'title': 'Финал лиги чемпионов',
+        'description': 'Реал опять выиграл ЛЧ',
+        'date': '28 мая 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+    },
+    {
+        'title': 'Охота на уток',
+        'description': 'Ни одна утка не пострадала',
+        'date': '7 дек 2019',
+        'content': 'Lorem ipsum dolor sit amet.'
+    },
+    {
+        'title': 'Фестиваль огурца',
+        'description': 'Суздаль ждет тебя',
+        'date': '12 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+    },
+    {
+        'title': 'Нашествие',
+        'description': 'Даешь рок, но в следующем году',
+        'date': '29 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+    },
+    {
+        'title': 'Новый год',
+        'description': 'Эх, еще один год пролетел',
+        'date': '31 дек 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+    },
+]
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'posts_lst': sample(posts_lst, 3)
+    }
+    return render(request, 'blog/index.html', context=context)
 
 
 def posts(request):
